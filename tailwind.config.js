82685d8bddf011419400ca8fa24a0fc112efb36f
Adrian Enachi <enachi.adrian52@gmail.com>
2024-11-07 13:25:09 +0100

    updated with react +tanstack router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
