708c7e257e9ebef758eb56e1921a2fc38f756635
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2024-11-19 22:08:40 +0100

    header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,57 @@
-
-import { Link } from '@tanstack/react-router';
-import { useAuth } from '../auth/AuthContext';
-import { singOut } from '../auth/utils';
-
+import { Link } from "@tanstack/react-router";
+import { useAuth } from "../auth/AuthContext";
+import { singOut } from "../auth/utils";
+import { Button } from "./Button";
+import { MdMenu } from "react-icons/md";
 
 const Header = () => {
-    const { user, setUser } = useAuth();
+  const { user, setUser } = useAuth();
 
+  return (
+    <header className="flex items-center justify-between p-4 bg-white shadow-md">
+      <div>
+        <h1 className="text-red font-header text-3xl font-bold">
+          Musik Samspil
+        </h1>
+        <p className="text-gray-800 font-body text-sm">
+          Skabt af DAOS - Dansk Amatørorkester Samvirke
+        </p>
+      </div>
 
+      <div className="cursor-pointer">
+        <MdMenu className="w-8 h-8 text-black" />{" "}
+        {/* Material Icon styled with Tailwind */}
+      </div>
 
-    return (
-        <header className="p-4 bg-gray-500">
-            {user ? (
-                <div className='flex flex-col items-start space-y-2'>
-                    <span className='italic'>Welcome, {user.name}</span>
-                    <Link to="/profile">
-                        <button className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Profile</button>
-                    </Link>
-                    <button className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' onClick={async () => await singOut(setUser)}>Logout</button>
-                </div>
-            ) : (
-                <div className='flex flex-col items-start space-y-2'>
-                    <span>Please log in</span>
-                    <div>
-                        <Link to="/login">
-                            <button className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Login</button>
-                        </Link>
-                        <Link to="/register">
-                            <button className='ml-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Register</button>
-                        </Link>
-                    </div>
-                    
-                </div>
-            )}
-        </header>
-    );
+      {user ? (
+        <div className="flex flex-col items-start space-y-2">
+          <h3 className="italic">Welcome, {user.name}</h3>
+          <Link to="/profile">
+            <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+              Profile
+            </button>
+          </Link>
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            onClick={async () => await singOut(setUser)}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col items-start space-y-2">
+          <div>
+            <Link to="/login">
+              <Button variant="secondary">Login</Button>
+            </Link>
+            <Link to="/register">
+              <Button>Register</Button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,63 +1,76 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router';
-import React, { useState } from 'react';
-import { useAuth } from '../auth/AuthContext';
+import { createFileRoute, Navigate } from "@tanstack/react-router";
+import React, { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
 
 export const Route = createFileRoute("/register")({
-    component: Register,
+  component: Register,
 });
 
 function Register() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const { user, setUser } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const { user, setUser } = useAuth();
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:3000/auth/register', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ name, email, password }),
-                credentials: 'include'
-            });
-            const data = await response.json();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+        credentials: "include",
+      });
+      const data = await response.json();
 
-            if (response.ok) {
-                setUser(data);
-                alert('Register successful');
-            } else {
-                alert(data.message)
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      if (response.ok) {
+        setUser(data);
+        alert("Register successful");
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    if(!user) {
-        return (
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Name:</label>
-                    <input type="name" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <br />
-    
-                <button className="underline" type="submit">Register</button>
-            </form>
-        );
-    } else {
-        return <Navigate to="/" />;
-    }   
-    
-};
+  if (!user) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <br />
+
+        <button className="underline" type="submit">
+          Register
+        </button>
+      </form>
+    );
+  } else {
+    return <Navigate to="/" />;
+  }
+}
