859811055e23a7c62a99b682f03141ece645f427
sofia amoroso <sofi79a0@stud.kea.dk>
2024-11-20 13:09:11 +0100

    font imported, hero almost done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { Link, createFileRoute } from "@tanstack/react-router";
 import { Button } from "../components/Button";
+import HeroImage from "../assets/hero-illustration.svg";
+import MusikerIcon from "../assets/musiker-icon.png";
+import EnsembleIcon from "../assets/ensemble-icon.png";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -7,10 +10,55 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-10">
-      <div className="card">
-        <p>hi, welcome, thats it</p>
+    <div>
+      <section id="hero" className="p-6">
+        <img
+          src={HeroImage}
+          alt="Hero illustration"
+          className="w-full h-auto p-10"
+        />
+        <h1 className="font-header text-red font-medium text-3xl">
+          Stedet hvor musikere finder musikere og spiller musik sammen
+        </h1>
 
+        <div className="flex space-x-4 ">
+          <button className="flex flex-col basis-1/2  items-center justify-center border rounded-lg border-gray-400 p-4 ">
+            <img
+              src={MusikerIcon}
+              alt="Musiker Icon"
+              className="w-8 h-auto mb-2"
+            />
+            <p className="font-body text-blue-800 text-sm font-bold">
+              Find musiker
+            </p>
+          </button>
+
+          <button className="flex flex-col basis-1/2 items-center justify-center border rounded-lg border-gray-400 p-4 ">
+            <img
+              src={EnsembleIcon}
+              alt="Ensemble Icon"
+              className="w-8 h-auto mb-2"
+            />
+            <p className="font-body text-blue-800 text-sm font-bold ">
+              Find ensemble
+            </p>
+          </button>
+        </div>
+      </section>
+
+      <section id="reviews" className="bg-blue-800 p-6">
+        <h2 className="font-header text-white font-medium text-3xl">
+          Det siger vores brugere
+        </h2>
+      </section>
+
+      <section id="latest-posts" className="p-6">
+        <h2 className="font-header text-blue-800 font-medium text-3xl">
+          Seneste opslag
+        </h2>
+      </section>
+
+      <div className="card">
         <div>
           <Link to="/react">
             <Button variant="primary">Components page</Button>
