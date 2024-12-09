3c853cc633a95d62362c227d18565b484dd72674
sofia amoroso <sofi79a0@stud.kea.dk>
2024-12-09 15:07:31 +0100

    small UI changes in postCard

--- src/components/PostCardMine.tsx
+++ src/components/PostCardMine.tsx
@@ -4,6 +4,7 @@ import EnsemblePortrait from '../assets/ensemble-portrait.jpeg';
 import { Post } from '../types/types';
 import { Button } from '../components/elements/Button';
 import MusicianPortrait from '../assets/musician-portrait.jpg';
+import MusicianIcon from '../assets/musiker-icon.png';
 
 export function PostCard({ post }: { post: Post }) {
 	const navigate = useNavigate();
@@ -20,7 +21,7 @@ export function PostCard({ post }: { post: Post }) {
 
 	return (
 		<article onClick={handleNavigate} className="border-2 border-gray-400 rounded-xl shadow-sm hover:shadow-md transition cursor-pointer">
-			<div className="flex flex-row gap-4 justify-between bg-gray-200 pt-4 px-4 relative">
+			<div className="flex flex-row gap-4 justify-between bg-gray-200 p-4 px-4 relative">
 				<img src={post.ensemble ? EnsemblePortrait : MusicianPortrait} alt="propic" className="w-[50px] h-[50px] object-cover rounded-lg" />
 
 				<div className="grow">
@@ -43,24 +44,26 @@ export function PostCard({ post }: { post: Post }) {
 								</p>
 							</div>
 
-							{location.pathname !== `/user/${post.author._id}` && location.pathname !== '/profile' && (
+							{location.pathname !== `/user/${post.author._id}` && location.pathname !== '/profile' ? (
 								<Button variant="tertiary" onClick={(e) => navigateProfile(e)}>
 									See profile
 								</Button>
+							) : (
+								<img src={MusicianIcon} alt="musician icon" className="self-end opacity-20 absolute bottom-2 right-4" />
 							)}
 						</div>
 					)}
 				</div>
 
 				{/* Render icon only for ensembles */}
-				{post.ensemble && <img src={EnsembleIcon} alt="ensemble icon" className="self-end opacity-20 absolute bottom-0 right-4" />}
+				{post.ensemble && <img src={EnsembleIcon} alt="ensemble icon" className="self-end opacity-20 absolute bottom-2 right-4" />}
 			</div>
 
-			<h4 className="font-body text-xl font-bold text-blue-800 p-4">{post.title}</h4>
+			<h4 className="font-body text-lg font-bold text-blue-800 p-4">{post.title}</h4>
 
 			<div className="flex flex-row justify-between px-4 pb-4">
-				<h5 className="font-header text-xl text-blue font-bold">{post.instrument?.name}</h5>
-				<p className="font-body text-md text-gray-800 font-bold">
+				<h5 className="font-header text-xl text-blue-800 font-bold">{post.instrument?.name}</h5>
+				<p className="font-body text-md text-gray-800 font-semibold">
 					Erfaring
 					<span className="text-white bg-blue-800 p-2 rounded-xl ml-2">{post.instrument?.level}+</span>
 				</p>
